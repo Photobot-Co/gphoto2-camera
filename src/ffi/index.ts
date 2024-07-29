1f3be332c62993cfb9f3cec5702d1cb70274b295
Matt Oakes <hello@mattoakes.net>
2024-07-29 12:04:29 +0100

    Add search paths for for linux libraries

--- src/ffi/index.ts
+++ src/ffi/index.ts
@@ -3,12 +3,21 @@ import { setupDefinitions } from "./definitions";
 import { setupFunctions } from "./functions";
 
 const POSSIBLE_LIBGPHOTO2_PATHS = [
+  // Linux
+  "/usr/lib/aarch64-linux-gnu/libgphoto2.so.6",
+  "libgphoto2.so.6",
+  // macOS
   "libgphoto2.dylib",
   "/opt/homebrew/lib/libgphoto2.dylib",
   "/usr/local/lib/libgphoto2.dylib",
 ];
 
-const POSSIBLE_LIBC_PATHS = ["libc.dylib"];
+const POSSIBLE_LIBC_PATHS = [
+  // Linux
+  "libc.so.6",
+  // macOS
+  "libc.dylib",
+];
 
 let ffi:
   | (ReturnType<typeof setupDefinitions> & ReturnType<typeof setupFunctions>)
